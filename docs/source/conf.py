624d6cf534224ba7dee0215e05c94c1efcab1d61
Edinyanga O. Ottoho <elzucky@gmail.com>
2022-06-24 15:00:41 +0100

    ascbuilder docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ASC Builder'
+copyright = '2022, AlgoKnox'
+author = 'AlgoKnox'
 
 release = '0.1'
 version = '0.1.0'
